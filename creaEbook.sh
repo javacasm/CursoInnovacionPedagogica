5a42f20d3351c50a05855f8e0742c659d74d81a1
javacasm <javacasm@gmail.com>
2017-05-29 13:45:16 +0200

    v0

---
+++ creaEbook.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TEMP_DIR="ebook-temp"
+TEMP_file=$TEMP_DIR"/ebook.md"
+
+EBOOK_NAME=$TEMP_DIR"/Raspberry"
+
+rm -rf $TEMP_DIR
+mkdir $TEMP_DIR
+
+files=( Cabecera.md        \
+intro.md \
+moda_robotica.md \
+makers.md \
+Proyectos.md \
+granadaMagica.md \
+Metodo.md \
+RedesSociales.md \
+programacion.md \
+herramientas.md \
+Robots.md \
+Equipos.md \
+EjemploBitbloq.md \
+3d.md \
+Impresion3D.md \
+Herramientas3D.md \
+OpenSource.md \
+Comprar.md )
+
+
+
+for i in "${files[@]}"
+do :
+   grep -v  -E '@javacasm|CCby|Módulo de Programación|Curso Superior de Innovación|<!--' $i >>$TEMP_file
+done
+
+
+pandoc --verbose -o $EBOOK_NAME".pdf" $TEMP_file
+mv $EBOOK_NAME* .
+
+
+# rm -rf $TEMP_DIR # comentado para comprobar
